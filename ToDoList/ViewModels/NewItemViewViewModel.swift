6eb433344a15574e5fc60ea278035881e853e2f1
AdishDevs <adishkanimsara.adikari@gmail.com>
2025-01-26 13:29:47 +0530

    Add ViewModel to save new to-do items - Added `NewItemViewViewModel` to handle saving new to-do items. - Included validation for title and due date. - Integrated Firebase Authentication and Firestore to save items under the user's todos.

--- ToDoList/ViewModels/NewItemViewViewModel.swift
+++ ToDoList/ViewModels/NewItemViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Admin on 2025-01-22.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject{
@@ -16,7 +17,31 @@ class NewItemViewViewModel: ObservableObject{
     init(){}
     
     func save(){
+        guard canSave else{
+            return
+        }
+        //Get current userId
+        guard let uId = Auth.auth().currentUser?.uid else{
+            return
+        }
+        //Create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false
+        )
         
+        
+        //Save model
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document(newId)
+            .setData(newItem.asDictionary())
     }
     
     var canSave: Bool {
